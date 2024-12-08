25eaec985a77d1cd908577f4051f9b510408f283
PeterVajda2 <novinar.peter@gmail.com>
2024-12-08 19:02:30 +0100

    Added DB tables for basic translations of products and categories. Added config system

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,9 @@
 use app::db_connection;
-use ntex::web;
+use app::CONFIG;
+use entity::user::{self, Entity as User};
+use migration::{Migrator, MigratorTrait};
+use ntex::web::{self};
+use sea_orm::EntityTrait;
 
 #[web::get("/")]
 async fn hello() -> impl web::Responder {
@@ -14,18 +18,20 @@ async fn echo(req_body: String) -> impl web::Responder {
 }
 
 async fn manual_hello() -> impl web::Responder {
-    web::HttpResponse::Ok().body("Hey there!")
+    let user: Option<user::Model> = User::find().one(db_connection()).await.unwrap();
+    web::HttpResponse::Ok().body(format!("{user:?}"))
 }
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
+    Migrator::refresh(db_connection()).await.unwrap();
     web::HttpServer::new(|| {
         web::App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((format!("{}", CONFIG.server.address), CONFIG.server.port))?
     .run()
     .await
 }
